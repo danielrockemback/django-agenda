7cc69d6d9a348631192911931dde6afcaea68fcf
Daniel Borges <d.rockemback@gmail.com>
2023-11-09 17:22:51 -0300

    Class Category e ForeignKey

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, Category
 
 
 @admin.register(Contact)
@@ -11,4 +11,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
-    
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
